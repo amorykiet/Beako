c53d4f7fa26cc4883fdea501a5b42dc9396313a9
amorykiet <amorykiet@gmail.com>
2023-05-12 08:38:39 +0700

    add delay after dashing

--- Subaru.gd
+++ Subaru.gd
@@ -14,6 +14,7 @@ var updating: bool= false
 var can_dash:= true
 var dash_direction: Vector2= Vector2.RIGHT
 var cur_stama: int = MAX_STAMA
+var fruit_collected: int = 0
 
 func _ready():
 	state.enter(self)
